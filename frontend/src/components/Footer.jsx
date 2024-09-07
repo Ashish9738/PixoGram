0fc24a5ecb20459326d795262bf96db1a071b5cf
Ashish9738 <ayadav9738@gmail.com>
2024-09-07 11:47:17 +0530

    Added Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="mt-8 w-full bg-black px-8 px8 md:px-[500px] flex justify-between text-sm  md:text-md py-8">
+      <div className="flex flex-col text-white">
+        <p>All rights reserved @PixoGram 2024</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
